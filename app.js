bbe7df5ba9a98633e43ed224b5b3838c338f29be
Tambass <tambass85@gmail.com>
2020-09-07 16:54:03 +0200

    add & delete factory or energy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const methodOverride = require("method-override");
 const path = require("path");
 const mysql = require("mysql");
+const util = require("util");
 const PORT = 8000;
 const app = express();
 app.set("view engine", "ejs");
@@ -24,27 +25,59 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATA,
-  multipleStatements: true,
+  // multipleStatements: true,
 });
+
 db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log("Connecté à la base MySQL");
 });
+const query = util.promisify(db.query).bind(db);
 global.db = db;
-
+global.query = query;
 // Controllers
 
 // Routes
 
-app.get("/", (req, res) => {
-  const query = ["SELECT * FROM factories", "SELECT * FROM energies"];
+app.get("/", async (req, res) => {
+  //const query = ["SELECT * FROM factories", "SELECT * FROM energies"];
 
-  db.query(query.join(";"), (err, result) => {
-    console.log(result);
-    res.render("index", { factories: result[0], energies: result[1] });
-  });
+  const factories = await query("SELECT * FROM factories");
+  const energies = await query("SELECT * FROM energies");
+
+  console.log(factories);
+
+  res.render("index", { factories, energies });
+
+  // db.query(query, (err, result) => {
+  // console.log(result);
+  //res.render("index", { factories: result[0], energies: result[1] });
+  //});
+});
+
+app.post("/add", async (req, res) => {
+  const factoryName = req.body.factoryName;
+  const energyName = req.body.energyName;
+  const addFactory = await query(
+    "INSERT INTO factories (name) VALUES ('" + factoryName + "')"
+  );
+  const addEnergy = await query(
+    "INSERT INTO energies (name) VALUES ('" + energyName + "')"
+  );
+
+  res.redirect("/");
+});
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const deleteFactory = await query("DELETE FROM factories WHERE id =" + id);
+
+  const deleteEnergy = await query("DELETE FROM energies WHERE id =" + id);
+
+  res.redirect("/");
 });
 
 // SERVER
