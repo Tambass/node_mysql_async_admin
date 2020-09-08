534281fb696f3ece4f3bdb9601ec1d0eab887a44
Tambass <tambass85@gmail.com>
2020-09-08 12:20:23 +0200

    upload images & display them

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const methodOverride = require("method-override");
 const path = require("path");
 const mysql = require("mysql");
@@ -11,6 +12,9 @@ require("dotenv").config();
 
 // Method OverRide
 app.use(methodOverride("_method"));
+
+// default options
+app.use(fileUpload());
 
 // Middleware
 app.use(express.json());
@@ -46,10 +50,11 @@ app.get("/", async (req, res) => {
 
   const factories = await query("SELECT * FROM factories");
   const energies = await query("SELECT * FROM energies");
+  const cars = await query("SELECT name, image FROM cars");
 
   console.log(factories);
 
-  res.render("index", { factories, energies });
+  res.render("index", { factories, energies, cars });
 
   // db.query(query, (err, result) => {
   // console.log(result);
@@ -67,6 +72,52 @@ app.post("/add", async (req, res) => {
     "INSERT INTO energies (name) VALUES ('" + energyName + "')"
   );
 
+  res.redirect("/");
+});
+
+app.post("/addFile", async (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const imageUpload = req.files.image;
+
+  const name = req.body.name;
+
+  const image = `images/${imageUpload.name}`;
+
+  try {
+    if (
+      imageUpload.mimetype === "image/jpeg" ||
+      imageUpload.mimetype === "image/jpg" ||
+      imageUpload.mimetype === "image/gif" ||
+      imageUpload.mimetype === "image/png"
+    ) {
+      imageUpload.mv(`public/images/${imageUpload.name}`, async function (err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
+
+        try {
+          await query("INSERT INTO cars (name, image) VALUES (?, ?);", [
+            name,
+            image,
+          ]);
+        } catch (err) {
+          res.send(err);
+        }
+      });
+    } else {
+      message = "Fichier invalide";
+      res.render("/add", { message });
+    }
+
+    //console.log(imageUpload);
+    await query;
+    res.send("ok !");
+  } catch (err) {
+    res.send(err);
+  }
   res.redirect("/");
 });
 
